78a9fd92d54669707601b256cf220d78c4195a9f
Pranjal Tandon <pranjal.tandon@gmail.com>
2018-09-02 00:08:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -29,18 +29,27 @@ class QNetwork(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size):
         super(QNetwork, self).__init__()
 
+        # Q1 architecture
         self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, 1)
 
+        # Q2 architecture
+        self.linear4 = nn.Linear(num_inputs + num_actions, hidden_size)
+        self.linear5 = nn.Linear(hidden_size, hidden_size)
+        self.linear6 = nn.Linear(hidden_size, 1)
+
     def forward(self, state, action):
-        x = torch.cat([state, action], 1)
-        x = F.relu(self.linear1(x))
-        x = F.relu(self.linear2(x))
-        x = self.linear3(x)
-        return x
+        x1 = torch.cat([state, action], 1)
+        x1 = F.relu(self.linear1(x1))
+        x1 = F.relu(self.linear2(x1))
+        x1 = self.linear3(x1)
 
-
+        x2 = torch.cat([state, action], 1)
+        x2 = F.relu(self.linear4(x2))
+        x2 = F.relu(self.linear5(x2))
+        x2 = self.linear6(x2)
+        return x1, x2
 class GaussianPolicy(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size):
         super(GaussianPolicy, self).__init__()

2b4dc127690b42a85d477ee02d94113cf51a62ab
Pranjal Tandon <pranjal.tandon@gmail.com>
2018-09-04 00:55:19 +0530

    Update model.py

--- model.py
+++ model.py
@@ -9,6 +9,11 @@ from utils import create_log_gaussian, logsumexp
 LOG_SIG_MAX = 2
 LOG_SIG_MIN = -20
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        torch.nn.init.normal_(m.weight, mean=0, std=0.1)
+
 
 class ValueNetwork(nn.Module):
     def __init__(self, state_dim, hidden_dim):
@@ -17,6 +22,7 @@ class ValueNetwork(nn.Module):
         self.linear1 = nn.Linear(state_dim, hidden_dim)
         self.linear2 = nn.Linear(hidden_dim, hidden_dim)
         self.linear3 = nn.Linear(hidden_dim, 1)
+
 
     def forward(self, state):
         x = F.relu(self.linear1(state))
@@ -39,6 +45,7 @@ class QNetwork(nn.Module):
         self.linear5 = nn.Linear(hidden_size, hidden_size)
         self.linear6 = nn.Linear(hidden_size, 1)
 
+
     def forward(self, state, action):
         x1 = torch.cat([state, action], 1)
         x1 = F.relu(self.linear1(x1))
@@ -49,9 +56,10 @@ class QNetwork(nn.Module):
         x2 = F.relu(self.linear4(x2))
         x2 = F.relu(self.linear5(x2))
         x2 = self.linear6(x2)
+
         return x1, x2
-    
-    
+
+
 class GaussianPolicy(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size):
         super(GaussianPolicy, self).__init__()
@@ -63,14 +71,14 @@ class GaussianPolicy(nn.Module):
 
         self.log_std_linear = nn.Linear(hidden_size, num_actions)
 
+        self.apply(weights_init)
+
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
-
         mean = self.mean_linear(x)
         log_std = self.log_std_linear(x)
         log_std = torch.clamp(log_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
-
         return mean, log_std
 
     def evaluate(self, state, reparam=False, epsilon=1e-6):
@@ -110,6 +118,8 @@ class GaussianMixturePolicy(nn.Module):
 
         self.out_linear = nn.Linear(hidden_size, (k * 2 * self.actions) + k)
 
+        self.apply(weights_init)
+
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
@@ -139,6 +149,7 @@ class GaussianMixturePolicy(nn.Module):
         normal = Normal(means, stds)
         x_t = normal.sample()
         action = torch.tanh(x_t)
+
 
         log_prob = create_log_gaussian(mean, log_std, x_t[:, None, :]) - torch.log(1 - action.pow(2) + epsilon).sum(
             dim=-1, keepdim=True)

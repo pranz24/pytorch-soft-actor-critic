1423a10727ed5646d13d3be38abafefa2a7bf7bc
Pranjal Tandon <pranjal.tandon@gmail.com>
2019-01-20 16:51:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ parser.add_argument('--seed', type=int, default=456, metavar='N',
                     help='random seed (default: 456)')
 parser.add_argument('--batch_size', type=int, default=256, metavar='N',
                     help='batch size (default: 256)')
-parser.add_argument('--num_steps', type=int, default=500001, metavar='N',
+parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
                     help='maximum number of steps (default: 1000000)')
 parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
                     help='hidden size (default: 256)')

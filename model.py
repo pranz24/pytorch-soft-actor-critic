9c2195168b90c24d8d79a29abc92d297e0622291
Pranjal Tandon <pranjal.tandon@gmail.com>
2019-02-20 14:41:50 +0530

    Update model.py

--- model.py
+++ model.py
@@ -10,14 +10,7 @@ LOG_SIG_MIN = -20
 epsilon = 1e-6
 
 # Initialize Policy weights
-def weights_init_policy_fn(m):
-    classname = m.__class__.__name__
-    if classname.find('Linear') != -1:
-        torch.nn.init.xavier_uniform_(m.weight, gain=0.5)
-        torch.nn.init.constant_(m.bias, 0)
-
-# Initialize Value Fn weights
-def weights_init_value_fn(m):
+def weights_init_(m):
     classname = m.__class__.__name__
     if classname.find('Linear') != -1:
         torch.nn.init.xavier_uniform_(m.weight, gain=1)
@@ -25,14 +18,14 @@ def weights_init_value_fn(m):
 
 
 class ValueNetwork(nn.Module):
-    def __init__(self, state_dim, hidden_dim):
+    def __init__(self, num_inputs, hidden_dim):
         super(ValueNetwork, self).__init__()
 
-        self.linear1 = nn.Linear(state_dim, hidden_dim)
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
         self.linear2 = nn.Linear(hidden_dim, hidden_dim)
         self.linear3 = nn.Linear(hidden_dim, 1)
 
-        self.apply(weights_init_value_fn)
+        self.apply(weights_init_)
 
     def forward(self, state):
         x = F.relu(self.linear1(state))
@@ -42,20 +35,20 @@ class ValueNetwork(nn.Module):
 
 
 class QNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_size):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
         super(QNetwork, self).__init__()
 
         # Q1 architecture
-        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
-        self.linear3 = nn.Linear(hidden_size, 1)
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, 1)
 
         # Q2 architecture
-        self.linear4 = nn.Linear(num_inputs + num_actions, hidden_size)
-        self.linear5 = nn.Linear(hidden_size, hidden_size)
-        self.linear6 = nn.Linear(hidden_size, 1)
+        self.linear4 = nn.Linear(num_inputs + num_actions, hidden_dim)
+        self.linear5 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear6 = nn.Linear(hidden_dim, 1)
 
-        self.apply(weights_init_value_fn)
+        self.apply(weights_init_)
 
     def forward(self, state, action):
         x1 = torch.cat([state, action], 1)
@@ -72,17 +65,16 @@ class QNetwork(nn.Module):
 
 
 class GaussianPolicy(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_size):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
         super(GaussianPolicy, self).__init__()
 
-        self.linear1 = nn.Linear(num_inputs, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
 
-        self.mean_linear = nn.Linear(hidden_size, num_actions)
+        self.mean_linear = nn.Linear(hidden_dim, num_actions)
+        self.log_std_linear = nn.Linear(hidden_dim, num_actions)
 
-        self.log_std_linear = nn.Linear(hidden_size, num_actions)
-
-        self.apply(weights_init_policy_fn)
+        self.apply(weights_init_)
 
     def forward(self, state):
         x = F.relu(self.linear1(state))
@@ -92,7 +84,7 @@ class GaussianPolicy(nn.Module):
         log_std = torch.clamp(log_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
         return mean, log_std
 
-    def evaluate(self, state):
+    def sample(self, state):
         mean, log_std = self.forward(state)
         std = log_std.exp()
         normal = Normal(mean, std)
@@ -101,29 +93,31 @@ class GaussianPolicy(nn.Module):
         log_prob = normal.log_prob(x_t)
         # Enforcing Action Bound
         log_prob -= torch.log(1 - action.pow(2) + epsilon)
-        log_prob = log_prob.sum(-1, keepdim=True)
+        log_prob = log_prob.sum(1, keepdim=True)
         return action, log_prob, x_t, mean, log_std
 
 class DeterministicPolicy(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_size):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
         super(DeterministicPolicy, self).__init__()
-        self.linear1 = nn.Linear(num_inputs, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
 
-        self.mean = nn.Linear(hidden_size, num_actions)
+        self.mean = nn.Linear(hidden_dim, num_actions)
         self.noise = torch.Tensor(num_actions)
 
-        self.apply(weights_init_policy_fn)
+        self.apply(weights_init_)
 
-    def forward(self, inputs):
-        x = inputs
-        x = F.relu(self.linear1(x))
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
-        mean = F.tanh(self.mean(x))
+        mean = torch.tanh(self.mean(x))
         return mean
 
 
-    def evaluate(self, state):
+    def sample(self, state):
         mean = self.forward(state)
-        action = mean + self.noise.normal_(0., std=0.015)
+        noise = self.noise.normal_(0., std=0.1)
+        noise = noise.clamp(-0.25, 0.25)
+        action = mean + noise
         return action, torch.tensor(0.), torch.tensor(0.), mean, torch.tensor(0.)
+    
